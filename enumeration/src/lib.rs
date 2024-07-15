542ee97a3372a2b73b4d3b52e2e48605adea468b
Joshua Booth <joshua.n.booth@gmail.com>
2024-07-15 16:23:39 -0700

    0.5.3

--- enumeration/src/lib.rs
+++ enumeration/src/lib.rs
@@ -1,11 +1,15 @@
 #[cfg(not(test))]
-#[cfg(feature = "derive")]
+#[cfg(feature = "enumeration_derive")]
 extern crate enumeration_derive;
 
 #[cfg(test)]
-#[cfg(feature = "derive")]
+#[cfg(feature = "enumeration_derive")]
 #[macro_use]
 extern crate enumeration_derive;
+
+#[cfg(feature = "enumeration_derive")]
+#[cfg_attr(docsrs, doc(cfg(feature = "derive")))]
+pub use enumeration_derive::Enum;
 
 #[macro_use]
 mod enum_trait;
